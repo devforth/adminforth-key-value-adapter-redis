e100ad18ff732043175e7b341fd2d6700848c4c7
yaroslav8765 <pechorkin2014@gmail.com>
2026-02-02 08:15:45 +0200

    feat: implement Redis integration in RAMKeyValueAdapter and update dependencies

--- index.ts
+++ index.ts
@@ -1,24 +1,54 @@
 import type { KeyValueAdapter } from "adminforth";
+import { createClient } from 'redis';
+import { AdapterOptions } from "./types.js";
 
 export default class RAMKeyValueAdapter implements KeyValueAdapter {
   private data: Map<string, any>;
 
-  constructor() {
-    this.data = new Map();
+  private memory: Map<string, any>;
+  private redis: ReturnType<typeof createClient>;
+  options: AdapterOptions;
+
+  constructor(options: AdapterOptions) {
+    this.memory = new Map();
+    this.redis = createClient({ url: options.redisUrl });
+
+    this.redis.on('error', (err) => console.error('Redis error', err));
+
+    this.redis.connect();
+    this.options = options;
   }
 
   validate() {
 
   }
 
-  async get(key: string): Promise<string> {
+ async set(key, value, expiresInSeconds?) {
+    if (expiresInSeconds) {
+      await this.redis.set(key, value, { EX: expiresInSeconds });
+    } else {
+      await this.redis.set(key, value);
+    }
   }
 
-  async set(key: string, value: any, expiresInSeconds?: number): Promise<void> {
+  async get(key) {
+    if (this.memory.has(key)) {
+      return this.memory.get(key);
+    }
 
+    const redisValue = await this.redis.get(key);
+    if (redisValue) {
+      const parsed = JSON.parse(redisValue);
+      this.memory.set(key, parsed);
+      return parsed;
+    }
+
+    return null;
   }
 
-  async delete(key: string): Promise<void> {
-    
+  async delete(key) {
+    this.memory.delete(key);
+    await this.redis.del(key);
   }
-}
+
+  }
--- types.ts
+++ types.ts
@@ -1,3 +1,4 @@
 export interface AdapterOptions {
-  
+  persist: boolean;
+  redisUrl: string;
 }
